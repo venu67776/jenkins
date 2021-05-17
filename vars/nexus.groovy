9b97ec44fcabed23757965461fc54bcd45c30d71
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-05-17 23:56:17 +0530

    new

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -42,7 +42,7 @@ def code_build(APP_TYPE, COMPONENT) {
     print execute_com
   }
    else if(APP_TYPE == "GOLANG") {
-    command = "go build"
+    command = "export GOPATH=/go && go build"
     def execute_com=sh(returnStdout: true, script: command)
     print execute_com
   }
