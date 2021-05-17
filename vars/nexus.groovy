628661f1eef9099cc1c542ec1ecc2fd42a6054ed
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-05-17 16:51:33 +0530

    new

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -3,7 +3,7 @@ def nexus(COMPONENT) {
   def get_branch_exec=sh(returnStdout: true, script: get_branch)
   def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
 
-  command = "curl -f -v -u admin:admin123 --upload-file ${FILENAME} http://172.31.3.145:8081/repository/${COMPONENT}/${FILENAME}"
+  command = "curl -f -v -u admin:admin --upload-file ${FILENAME} http://172.31.3.145:8081/repository/${COMPONENT}/${FILENAME}"
   def execute_state=sh(returnStdout: true, script: command)
 }
 
@@ -25,7 +25,7 @@ def make_artifacts(APP_TYPE, COMPONENT) {
     def execute_com=sh(returnStdout: true, script: command)
     print execute_com
    } else if(APP_TYPE == "GOLANG") {
-    command = "zip -r ${FILENAME} main.go tracing.go user.go "
+    command = "zip -r ${FILENAME} main.go tracing.go user.go"
     def execute_com=sh(returnStdout: true, script: command)
     print execute_com
   }
