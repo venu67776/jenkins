48fc60c7c052ae5762ea3756e426d27418d72ff1
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-05-18 21:41:45 +0530

    new

--- seed.groovy
+++ seed.groovy
@@ -2,33 +2,18 @@ folder('CI-Pipelines') {
   displayName('CI Pipelines')
   description('CI Pipelines')
 }
-
-def component = ["users","login","todo","frontend"];
-
-def count=(component.size()-1)
-for (i in 0..count) {
-  def j=component[i]
-  pipelineJob("CI-Pipelines/${j}-ci") {
+pipelineJob('frontend-ci') {
     configure { flowdefinition ->
-      flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
-        'triggers' {
-          'hudson.triggers.SCMTrigger' {
-            'spec'('*/2 * * * 1-5')
-            'ignorePostCommitHooks'(false)
-          }
-        }
-      }
       flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
         'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
           'userRemoteConfigs' {
             'hudson.plugins.git.UserRemoteConfig' {
-              'url'('https://github.com/venu67776/'+j+'.git')
-              'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+              'url'('https://github.com/venu67776/frontend.git')
             }
           }
           'branches' {
             'hudson.plugins.git.BranchSpec' {
-              'name'('*/tags/*')
+              'name'('*/main')
             }
           }
         }
@@ -37,4 +22,3 @@ for (i in 0..count) {
       }
     }
   }
-}
