2c32a7d035d636fa6b1f47f884a6cc88c812dd94
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-05-19 22:13:13 +0530

    new

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -25,7 +25,7 @@ def make_artifacts(APP_TYPE, COMPONENT) {
     def execute_com=sh(returnStdout: true, script: command)
     print execute_com
   } else if(APP_TYPE == "GOLANG") {
-    command = "zip -r ${FILENAME} login main.go user.go tracking.go"
+    command = "zip -r ${FILENAME} login main.go user.go tracing.go"
     def execute_com=sh(returnStdout: true, script: command)
     print execute_com
   }
