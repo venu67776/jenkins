2d002770808b8269834cb3b196daaaf3cf9b4b22
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-05-19 10:28:48 +0530

    new

--- seed.groovy
+++ seed.groovy
@@ -4,13 +4,81 @@ folder('CI-Pipelines') {
 }
 
 
-pipelineJob('CI-Pipelines/frontend-ci') {
+pipelineJob('CI-Pipelines/login-ci') {
+    configure { flowdefinition ->
+      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'('https://github.com/venu67776/login.git')
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/main')
+            }
+          }
+        }
+        'scriptPath'('Jenkinsfile')
+        'lightweight'(true)
+      }
+    }
+  }
+
+
+
+
+  pipelineJob('CI-Pipelines/frontend-ci') {
     configure { flowdefinition ->
       flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
         'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
           'userRemoteConfigs' {
             'hudson.plugins.git.UserRemoteConfig' {
               'url'('https://github.com/venu67776/frontend.git')
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/main')
+            }
+          }
+        }
+        'scriptPath'('Jenkinsfile')
+        'lightweight'(true)
+      }
+    }
+  }
+
+
+  pipelineJob('CI-Pipelines/users-ci') {
+    configure { flowdefinition ->
+      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'('https://github.com/venu67776/users.git')
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/main')
+            }
+          }
+        }
+        'scriptPath'('Jenkinsfile')
+        'lightweight'(true)
+      }
+    }
+  }
+
+
+  pipelineJob('CI-Pipelines/todo-ci') {
+    configure { flowdefinition ->
+      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'('https://github.com/venu67776/todo.git')
             }
           }
           'branches' {
