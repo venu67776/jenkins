446f0116dcbf80063e8c094efbfd5e1ae09eb431
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-05-20 10:35:45 +0530

    n

--- seed.groovy
+++ seed.groovy
@@ -3,7 +3,7 @@ folder('CI-Pipelines') {
   description('CI Pipelines')
 }
 
-def component = ["cart", "catalogue","user","payment","shipping","frontend"];
+def component = ["user","login","todo","frontend"];
 
 def count=(component.size()-1)
 for (i in 0..count) {
