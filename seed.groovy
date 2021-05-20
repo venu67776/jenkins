40b082e5b0e9f5f6034b67817d41dd6e85538103
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-05-20 10:34:14 +0530

    new

--- seed.groovy
+++ seed.groovy
@@ -3,19 +3,31 @@ folder('CI-Pipelines') {
   description('CI Pipelines')
 }
 
+def component = ["cart", "catalogue","user","payment","shipping","frontend"];
 
-pipelineJob('CI-Pipelines/login-ci') {
+def count=(component.size()-1)
+for (i in 0..count) {
+  def j=component[i]
+  pipelineJob("CI-Pipelines/${j}-ci") {
     configure { flowdefinition ->
+      flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+        'triggers' {
+          'hudson.triggers.SCMTrigger' {
+            'spec'('*/2 * * * 1-5')
+            'ignorePostCommitHooks'(false)
+          }
+        }
+      }
       flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
         'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
           'userRemoteConfigs' {
             'hudson.plugins.git.UserRemoteConfig' {
-              'url'('https://github.com/venu67776/login.git')
+              'url'('https://github.com/zsdevops01/'+j+'.git')
             }
           }
           'branches' {
             'hudson.plugins.git.BranchSpec' {
-              'name'('*/main')
+              'name'('*/tags/*')
             }
           }
         }
@@ -24,71 +36,4 @@ pipelineJob('CI-Pipelines/login-ci') {
       }
     }
   }
-
-
-
-
-  pipelineJob('CI-Pipelines/frontend-ci') {
-    configure { flowdefinition ->
-      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-          'userRemoteConfigs' {
-            'hudson.plugins.git.UserRemoteConfig' {
-              'url'('https://github.com/venu67776/frontend.git')
-            }
-          }
-          'branches' {
-            'hudson.plugins.git.BranchSpec' {
-              'name'('*/main')
-            }
-          }
-        }
-        'scriptPath'('Jenkinsfile')
-        'lightweight'(true)
-      }
-    }
-  }
-
-
-  pipelineJob('CI-Pipelines/users-ci') {
-    configure { flowdefinition ->
-      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-          'userRemoteConfigs' {
-            'hudson.plugins.git.UserRemoteConfig' {
-              'url'('https://github.com/venu67776/users.git')
-            }
-          }
-          'branches' {
-            'hudson.plugins.git.BranchSpec' {
-              'name'('*/main')
-            }
-          }
-        }
-        'scriptPath'('Jenkinsfile')
-        'lightweight'(true)
-      }
-    }
-  }
-
-
-  pipelineJob('CI-Pipelines/todo-ci') {
-    configure { flowdefinition ->
-      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-          'userRemoteConfigs' {
-            'hudson.plugins.git.UserRemoteConfig' {
-              'url'('https://github.com/venu67776/todo.git')
-            }
-          }
-          'branches' {
-            'hudson.plugins.git.BranchSpec' {
-              'name'('*/main')
-            }
-          }
-        }
-        'scriptPath'('Jenkinsfile')
-        'lightweight'(true)
-      }
-    }
-  }
+}
