ecae10ce0a4e2761727126db4a8bc1d101a8c4db
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-05-24 15:41:33 +0530

    new

--- seed.groovy
+++ seed.groovy
@@ -3,7 +3,7 @@ folder('CI-Pipelines') {
   description('CI Pipelines')
 }
 
-def component = ["users","login","todo","frontend"];
+def component = ["todo","users","login","frontend"];
 
 def count=(component.size()-1)
 for (i in 0..count) {
