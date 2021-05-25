497ad35860c7ebf4ba4b12c0df45c7857031afa2
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-05-25 12:34:02 +0530

    new

--- seed.groovy
+++ seed.groovy
@@ -23,12 +23,12 @@ for (i in 0..count) {
           'userRemoteConfigs' {
             'hudson.plugins.git.UserRemoteConfig' {
               'url'('https://github.com/venu67776/'+j+'.git')
-
+              'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')   
             }
           }
           'branches' {
             'hudson.plugins.git.BranchSpec' {
-              'name'('**/main')
+              'name'('*/tags/*')
             }
           }
         }
