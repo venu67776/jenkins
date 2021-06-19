6a0e0f858367148743144ac67e1c9a8300cfe911
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-06-19 09:23:52 +0530

    new

--- seed.groovy
+++ seed.groovy
@@ -30,7 +30,7 @@ for (i in 0..count) {
           }
           'branches' {
             'hudson.plugins.git.BranchSpec' {
-              'name'('*/main/*')
+              'name'('*/main')
             }
           }
         }
