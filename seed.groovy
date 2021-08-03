ee6e7335e98fa609f99abd9fe7d837f4cb5201f8
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-08-03 11:42:33 +0530

    new

--- seed.groovy
+++ seed.groovy
@@ -35,7 +35,7 @@ for (i in 0..count) {
             }
           }
         }
-        'scriptPath'('Jenkinsfile')
+        'scriptPath'('Jenkinsfile-Docker')
         'lightweight'(true)
       }
     }
---
+++ vars/docker.groovy
@@ -0,0 +1,76 @@
+def call(Map params = [:]) {
+  // Start Default Arguments
+  def args = [
+      SLAVE_LABEL : "DOCKER"
+  ]
+  args << params
+
+  // End Default + Required Arguments
+  pipeline {
+    agent {
+    node{
+        label "${args.SLAVE_LABEL}"
+    }
+      
+    }
+
+    triggers {
+      pollSCM('* * * * 1-5')
+    }
+
+    environment {
+      COMPONENT     = "${args.COMPONENT}"
+      PROJECT_NAME  = "${args.PROJECT_NAME}"
+      SLAVE_LABEL   = "${args.SLAVE_LABEL}"
+      APP_TYPE      = "${args.APP_TYPE}"
+    }
+
+    stages {
+
+      stage('Docker Build') {
+        steps {
+          script {
+            get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
+            env.get_branch_exec=sh(returnStdout: true, script: get_branch)
+          }
+          sh '''
+            aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 488617992296.dkr.ecr.us-east-1.amazonaws.com
+            docker build -t 488617992296.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${get_branch_exec} .
+          '''
+        }
+      }
+
+
+      stage('Docker push') {
+        steps {
+          script {
+            prepare = new nexus()
+            prepare.make_artifacts("${APP_TYPE}", "${COMPONENT}")
+          }
+        }
+      }
+
+      stage('Upload Artifacts') {
+        steps {
+          script {
+            prepare = new nexus()
+            prepare.nexus(COMPONENT)
+          }
+        }
+      }
+
+      stage('Deploy to Dev Env') {
+        steps {
+          script {
+            get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
+            env.get_branch_exec=sh(returnStdout: true, script: get_branch)
+          }
+          build job: 'Deployment Pipeline', parameters: [string(name: 'ENV', value: 'dev'), string(name: 'COMPONENT', value: "${COMPONENT}"), string(name: 'VERSION', value: "${get_branch_exec}")]
+        }
+      }
+
+    }
+
+  }
+
+}
